342ef3a0de2359a60fb090dfd8330e3653aa1512
vladasp <vladalogin@ukr.net>
2015-08-25 12:28:33 +0300

    Refactoring according to professional coding style. Issue 1 items 1-5, 9, 11

--- src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
+++ src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
@@ -1,36 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleGeom
 {
     class ClassEvent
     {
-        public int ChangedLine()
+        public double ChangedLine()
         {
-            string a;
-            int b = 0; bool TRUE = false;
+            string parameterGet;
+            double parameterSet = 0;
+            bool checkParameter = false;
             do
             {
-                a = Console.ReadLine();
-                foreach (char c in a)
+                parameterGet = Console.ReadLine();
+                try
                 {
-                    if (Char.IsNumber(c) == true || c == '-')
-                    {
-                        b = int.Parse(a);
-                        TRUE = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Only numbers");
-                        a = a.Remove(a.Length - 1);
-                    }
+                    parameterSet = Convert.ToDouble(parameterGet);
+                    checkParameter = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Only numbers");
                 }
             }
-            while (TRUE == false);
-            return b;
-            }
+            while (!checkParameter);
+            return parameterSet;
         }
     }
+}
