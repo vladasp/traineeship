06d71ff8214d8b71f183b662f45579bc09365762
vladasp <vladalogin@ukr.net>
2015-08-26 15:35:09 +0300

    Replacement logic "Try Catch" to "If Else"

--- src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
+++ src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleGeom
 {
@@ -9,18 +10,61 @@ namespace ConsoleGeom
             string parameterGet;
             double parameterSet = 0;
             bool checkParameter = false;
+            List<char> myChar = new List<char>();
             do
             {
+                myChar.Clear();
                 parameterGet = Console.ReadLine();
-                try
+                foreach (char c in parameterGet)
                 {
-                    parameterSet = Convert.ToDouble(parameterGet);
-                    checkParameter = true;
+                    myChar.Add(c);
                 }
-                catch
+
+                #region Сheck the correctness of input
+                for (int i = 0; i < myChar.Count; i++)
                 {
-                    Console.WriteLine("Only numbers");
+                    for (int j = 0; j < myChar.Count; j++)
+                    {
+                        Console.WriteLine(myChar[i]);
+
+                        if (myChar[0] != '-' && myChar[i] != '-' && i > 0 && myChar[i] != ',' || myChar[0] == '-' && myChar[i] != '-' && i > 0 && myChar[i] != ',')
+                        {
+                            checkParameter = true;
+                        }
+                        else if (myChar[0] == '-' && myChar[1] != ',' && myChar[i] == ',' && i > 1 && j > 1 && myChar[i] != myChar[j] && i!=j)
+                        {
+                            checkParameter = true;
+                        }
+                        else if (myChar[0] == '-' && myChar[1] != ',' && i > 0 && j > 0 && myChar[i] != myChar[j] && i != j)
+                        {
+                            checkParameter = true;
+                        }
+                        else if (myChar[0] != '-' &&  myChar[0] == ',' && myChar[i] != myChar[j] && i != j)
+                        {
+                            checkParameter = true;
+                        }
+                        else if (Char.IsNumber(myChar[i]))
+                        {
+                            checkParameter = true;
+                        }
+                        else
+                        {
+                            checkParameter = false;
+                            break;
+                        }
+
+                    } if (!checkParameter) { break; }
                 }
+                #endregion
+
+                if (checkParameter)
+                    {
+                        parameterSet = Convert.ToDouble(parameterGet);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Only numbers");
+                    }
             }
             while (!checkParameter);
             return parameterSet;
