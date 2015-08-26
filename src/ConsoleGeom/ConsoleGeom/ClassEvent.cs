9f3da9f1451af246df0133c1274c288026c15bfb
vladasp <vladalogin@ukr.net>
2015-08-26 19:15:48 +0300

    Fix problems

--- src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
+++ src/ConsoleGeom/ConsoleGeom/ClassEvent.cs
@@ -7,67 +7,27 @@ namespace ConsoleGeom
     {
         public double ChangedLine()
         {
-            string parameterGet;
-            double parameterSet = 0;
-            bool checkParameter = false;
-            List<char> myChar = new List<char>();
+            string parameterSet;
+            double parameterGet = 0;
+            double number;
+            bool canPars = false;
+
             do
             {
-                myChar.Clear();
-                parameterGet = Console.ReadLine();
-                foreach (char c in parameterGet)
-                {
-                    myChar.Add(c);
-                }
-
-                #region Сheck the correctness of input
-                for (int i = 0; i < myChar.Count; i++)
-                {
-                    for (int j = 0; j < myChar.Count; j++)
+                parameterSet = Console.ReadLine();
+                    if (double.TryParse(parameterSet, out number))
                     {
-                        Console.WriteLine(myChar[i]);
-
-                        if (myChar[0] != '-' && myChar[i] != '-' && i > 0 && myChar[i] != ',' || myChar[0] == '-' && myChar[i] != '-' && i > 0 && myChar[i] != ',')
-                        {
-                            checkParameter = true;
-                        }
-                        else if (myChar[0] == '-' && myChar[1] != ',' && myChar[i] == ',' && i > 1 && j > 1 && myChar[i] != myChar[j] && i!=j)
-                        {
-                            checkParameter = true;
-                        }
-                        else if (myChar[0] == '-' && myChar[1] != ',' && i > 0 && j > 0 && myChar[i] != myChar[j] && i != j)
-                        {
-                            checkParameter = true;
-                        }
-                        else if (myChar[0] != '-' &&  myChar[0] == ',' && myChar[i] != myChar[j] && i != j)
-                        {
-                            checkParameter = true;
-                        }
-                        else if (Char.IsNumber(myChar[i]))
-                        {
-                            checkParameter = true;
-                        }
-                        else
-                        {
-                            checkParameter = false;
-                            break;
-                        }
-
-                    } if (!checkParameter) { break; }
-                }
-                #endregion
-
-                if (checkParameter)
-                    {
-                        parameterSet = Convert.ToDouble(parameterGet);
+                        parameterGet = Convert.ToDouble(parameterSet);
+                        canPars = true;
                     }
                     else
                     {
                         Console.WriteLine("Only numbers");
+                        canPars = false;
                     }
             }
-            while (!checkParameter);
-            return parameterSet;
+            while (!canPars);
+            return parameterGet;
         }
     }
 }
