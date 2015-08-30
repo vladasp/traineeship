304ee3353668da31c0fdeeb3c8f0f81150688232
vladasp <vladalogin@ukr.net>
2015-08-30 19:41:15 +0300

    Added new methods

---
+++ src/FileCatalog/ComplexNumbers/ComplexNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class ComplexNumbers
+    {
+        double a;
+        string bi;
+        public ComplexNumbers() { }
+        public ComplexNumbers(double value)
+        {
+            a = value;
+        }
+        public ComplexNumbers(string value)
+        {
+            bi = value;
+        }
+        public ComplexNumbers(double value1, string value2)
+        {
+            a = value1;
+            bi = value2;
+        }
+        public void NumToString()
+        {
+            string ai = Convert.ToString(a);
+            Console.Write("Number to string ({0}, {1})", ai, bi);
+            return;
+        }
+        public void GetMagnitude()
+        {
+            double b = Convert.ToDouble(bi.Remove(bi.Length - 1, 1));
+            double result = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Magnitude of complex nuber: {0}", Math.Round(result, 2));
+            return;
+        }
+        public void Add(double a2, string bi2)
+        {
+            a = a + a2;
+            double b = Convert.ToDouble(bi.Remove(bi.Length - 1, 1)) + Convert.ToDouble(bi2.Remove(bi2.Length - 1, 1));
+            bi = Convert.ToString(b) + "i";
+            Console.WriteLine("New complex number {0}", Convert.ToString(a) + bi);
+        }
+    }
+}
--- src/FileCatalog/ComplexNumbers/Program.cs
+++ src/FileCatalog/ComplexNumbers/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComplexNumbers
 {
     class Program
     {
+        enum Command { M = 1, S = 2, A = 3, E = 4 }
+
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            Console.WriteLine("Enter a");
+            double a = program.ChangedLine();
+            Console.WriteLine("Enter b");
+            string b = Convert.ToString(program.ChangedLine()) + "i";
+            ComplexNumbers complexNumber = new ComplexNumbers(a, b);
+            complexNumber.NumToString();
+
+            bool isExit = false;
+            string input;
+
+            while (!isExit)
+            {
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "M":
+                        {
+                            complexNumber.GetMagnitude();
+                            break;
+                        }
+                    case "S":
+                        {
+                            complexNumber.NumToString();
+                            break;
+                        }
+                    case "A":
+                        {
+                            Console.WriteLine("Enter real part of complex number");
+                            double a1 = program.ChangedLine();
+                            Console.WriteLine("Enter imaginary part of complex number");
+                            double b1 = program.ChangedLine();
+                            string bi1 = Convert.ToString(b1) + "i";
+                            complexNumber.Add(a1, bi1);
+                            break;
+                        }
+                    case "E":
+                        {
+                            isExit = true; break;
+                        }
+                    default: Instuctions(); break;
+
+
+                }
+            }
         }
+        public double ChangedLine()
+        {
+            string parameterSet;
+            double parameterGet = 0;
+            double number;
+            bool canPars = false;
+
+            do
+            {
+                parameterSet = Console.ReadLine();
+                if (double.TryParse(parameterSet, out number))
+                {
+                    parameterGet = Convert.ToDouble(parameterSet);
+                    canPars = true;
+                }
+                else
+                {
+                    Console.WriteLine("Only numbers");
+                    canPars = false;
+                }
+            }
+            while (!canPars);
+            return parameterGet;
+        }
+        static void Instuctions()
+        {
+            Console.WriteLine("||To magnitude number enter 'M',\n||to show number to string enter 'S',\n||to add complex number enter 'A',\n||to exit enter 'E'");
+        }
+
     }
 }
