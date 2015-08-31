39347fa0b25b023446871dd80efdb861efaf8641
vladasp <vladalogin@ukr.net>
2015-08-31 17:59:06 +0300

    Chapter4 Exercise3

---
+++ src/Beginning Visual C# Karli Watson/Chapter4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1 exercise1 = new Exercise1();
+            exercise1.IsMore();
+            Console.ReadKey();
+        }
+    }
+}
+
