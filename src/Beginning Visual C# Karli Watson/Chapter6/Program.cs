6d37958ec26fdd3a0824ec8750b12271756cef4a
vladasp <vladalogin@ukr.net>
2015-09-05 12:11:40 +0300

    Empty execises 1-5 of chapter6

--- src/Beginning Visual C# Karli Watson/Chapter6/Program.cs
+++ src/Beginning Visual C# Karli Watson/Chapter6/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Chapter5
+namespace Chapter6
 {
-    enum UserControl { Exercise1 = 1, Exercise2, Exercise3, Exercise4, Exercise5, Exercise6, Exercise7, Exit }
+    enum UserControl { Exercise1 = 1, Exercise2, Exercise3, Exercise4, Exercise5, Exit }
     class Program
     {
         static void Main(string[] args)
@@ -15,64 +15,47 @@ namespace Chapter5
             const int thirdControl = (int)UserControl.Exercise3;
             const int fourthControl = (int)UserControl.Exercise4;
             const int fifthControl = (int)UserControl.Exercise5;
-            const int sixthControl = (int)UserControl.Exercise6;
-            const int seventhControl = (int)UserControl.Exercise7;
-
 
             const int exitControl = (int)UserControl.Exit;
             while (!isExit)
             {
-                Console.WriteLine("|||For show solve of exercise from 1 to 7 enter relevant number,\n|||to exit enter 8");
+                Console.WriteLine("|||For show solve of exercise from 1 to 5 enter relevant number,\n|||to exit enter 6");
                 input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
                     case firstControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise1);
-                            Exercise1 exercise1 = new Exercise1();
-                            exercise1.Show();
+                            //Exercise1 exercise1 = new Exercise1();
+                            //exercise1.Show();
                             break;
                         }
                     case secondControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise2);
-                            Exercise2 exercise2 = new Exercise2();
-                            exercise2.Show();
+                            //Exercise2 exercise2 = new Exercise2();
+                            //exercise2.Show();
                             break;
                         }
                     case thirdControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise3);
-                            Exercise3 exercise3 = new Exercise3();
-                            exercise3.Show();
+                            //Exercise3 exercise3 = new Exercise3();
+                            //exercise3.Show();
                             break;
                         }
                     case fourthControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise4);
-                            Exercise4 exercise4 = new Exercise4();
-                            exercise4.Show();
+                            //Exercise4 exercise4 = new Exercise4();
+                            //exercise4.Show();
                             break;
                         }
                     case fifthControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise5);
-                            Exercise5 exercise5 = new Exercise5();
-                            exercise5.Show();
-                            break;
-                        }
-                    case sixthControl:
-                        {
-                            Console.WriteLine("Solve of {0}", UserControl.Exercise6);
-                            Exercise6 exercise6 = new Exercise6();
-                            exercise6.Show();
-                            break;
-                        }
-                    case seventhControl:
-                        {
-                            Console.WriteLine("Solve of {0}", UserControl.Exercise7);
-                            Exercise7 exercise7 = new Exercise7();
-                            exercise7.Show();
+                            //Exercise5 exercise5 = new Exercise5();
+                            //exercise5.Show();
                             break;
                         }
                     case exitControl:
@@ -85,7 +68,7 @@ namespace Chapter5
         }
         static void Instruction()
         {
-            Console.WriteLine("|||For show solve of exercise from 1 to 7 enter relevant number,\n|||to exit enter 8");
+            Console.WriteLine("|||For show solve of exercise from 1 to 5 enter relevant number,\n|||to exit enter 6");
             return;
         }
     }
