f25e482751c7da97348a3835a8a517a8837622a5
vladasp <vladalogin@ukr.net>
2015-09-07 23:50:17 +0300

    Empty chapter 8

--- src/Beginning Visual C# Karli Watson/Chapter8/Program.cs
+++ src/Beginning Visual C# Karli Watson/Chapter8/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Chapter6
+namespace Chapter8
 {
     enum UserControl { Exercise1 = 1, Exercise2, Exercise3, Exercise4, Exercise5, Exit }
     class Program
@@ -25,55 +25,36 @@ namespace Chapter6
                     case firstControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise1);
-                            Exercise1 exercise1 = new Exercise1();
-                            exercise1.Show();
+                            //Exercise1 exercise1 = new Exercise1();
+                            //exercise1.Show();
                             break;
                         }
                     case secondControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise2);
-                            Exercise2 exercise2 = new Exercise2();
-                            args[0] = "First argument";
-                            args[1] = "2";
-                            exercise2.Show(args[0], args[1]);
+                            //Exercise2 exercise2 = new Exercise2();
+                            //exercise2.Show(args[0], args[1]);
                             break;
                         }
                     case thirdControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise3);
-                            Exercise3 exercise3 = new Exercise3();
-                            SetChangeLine setChangeLine;
-                            exercise3.Show();
-                            setChangeLine = exercise3.SetConsoleReadLine;
-                            Console.WriteLine("Enter your name");
-                            string name = Console.ReadLine();
-                            Console.WriteLine("Enter your password");
-                            string password = Console.ReadLine();
-                            Console.WriteLine("Yours parameters to enter: {0}\n ", setChangeLine(name, password));
+                            //Exercise3 exercise3 = new Exercise3();
+                            //exercise3.Show();
                             break;
                         }
                     case fourthControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise4);
-                            Exercise4 exercise4 = new Exercise4();
-                            exercise4.Show();
-                            Order order1;
-                            order1.itemName = "Notebook";
-                            order1.unitCost = 500;
-                            order1.unitCount = 3;
-                            Console.WriteLine("Total sum {0} of units \"{1}\".\n ", order1.TotalSum(), order1.itemName);
+                            //Exercise4 exercise4 = new Exercise4();
+                            //exercise4.Show();
                             break;
                         }
                     case fifthControl:
                         {
                             Console.WriteLine("Solve of {0}", UserControl.Exercise5);
-                            Exercise5 exercise5 = new Exercise5();
-                            exercise5.Show();
-                            Order order2;
-                            order2.itemName = "Notebook";
-                            order2.unitCost = 500;
-                            order2.unitCount = 3;
-                            order2.Information();
+                            //Exercise5 exercise5 = new Exercise5();
+                            //exercise5.Show();
                             break;
                         }
                     case exitControl:
