7e9026380b86ac43febdfdd9f0374b73d6d1b510
vladasp <vladalogin@ukr.net>
2015-09-09 23:12:01 +0300

    Chapter9 exercises 1-3

---
+++ src/Beginning Visual C# Karli Watson/Chapter9/Exercise5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class Exercise5
+    {
+        public void Show()
+        {
+            Console.WriteLine(@"
+Создайте проект консольного приложения с именем Traffic, ссылающийся на проект 
+Vehicles.dll (из упражнения 4). Добавьте в него функцию AddPasenger, принимаю-
+щую любой объект с интерфейсом IPassengerCarrier. Для проверки работоспособно-
+сти кода вызовите эту функцию для экземпляров всех объектов, которые поддерживают 
+этот интерфейс, и выводите результаты с помощью метода ToString, унаследованного 
+от класса System.Object.
+                            ");
+        }
+
+    }
+}
