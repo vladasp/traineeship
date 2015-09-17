9996a305906cdea04f520f3af830af33bcbddc69
vladasp <vladalogin@ukr.net>
2015-09-17 11:04:27 +0300

    Empty chapter 11

---
+++ src/Beginning Visual C# Karli Watson/Chapter11/Exercise4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter11
+{
+    class Exercise4
+    {
+        public void Show()
+        {
+            Console.WriteLine(@"
+                            ");
+        }
+    }
+}
---
+++ src/Beginning Visual C# Karli Watson/Chapter11/Exercise5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter11
+{
+    class Exercise5
+    {
+        public void Show()
+        {
+            Console.WriteLine(@"
+                            ");
+        }
+    }
+}
